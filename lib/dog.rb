8f5a8c6792870f5def21d8be31c6ce9b85d0af6f
William George <42682665+wgeorge1990@users.noreply.github.com>
2019-05-10 12:59:23 -0400

    Update dog.rb

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,27 +1,21 @@
 require 'pry'
-
 class Dog
-
-
-  def name
-    @name
-  end
-
   def name= new_name
     @name=new_name
   end
-
+  
+   def name
+    @name
+  end
+  
+   def breed=new_breed
+    @breed = new_breed
+  end
+  
   def breed
     @breed
   end
-
-  def breed=new_breed
-    @breed = new_breed
-  end
-
 end
-
 spot = Dog.new
 spot.name
-
 puts spot
